bbe998267564b3b2e9a3517b88cc3d55b05951ed
Alexandre Gasnier <algasnie@2A2.42angouleme.fr>
2025-11-18 13:58:04 +0100

    Creation of the first stack of nodes with arguments

--- push_swap.c
+++ push_swap.c
@@ -6,102 +6,148 @@
 /*   By: algasnie <algasnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 14:41:38 by algasnie          #+#    #+#             */
-/*   Updated: 2025/11/17 17:41:11 by algasnie         ###   ########.fr       */
+/*   Updated: 2025/11/18 13:44:58 by algasnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
 #include "push_swap.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
-char *ft_atoi(char *argv)
+int ft_atoi(char *argv)
 {
+	int	i;
+	int sign;
+	int	number;
 
-
-
-	
+	i = 0;
+	number = 0;
+	sign = 1;
+	if (argv[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (argv[0] == '+')
+		i++;
+	while (argv[i])
+	{
+		if (number > INT_MAX / 10 || (argv[i] < '0' || argv[i] > '9'))
+			return (123); ////////////////////////////////////////////////////////////////////// a ;odifie
+		number *= 10;
+		number += argv[i] - '0';
+		i++;
+	}
+	number *= sign;
+	return (number);
 }
 
-int	ft_create_end_node(char **argv)
+int	ft_create_end_node(t_stack **first_node, int content)
 {
-	t_stack	*node;
+	t_stack	*new_node;
+	t_stack *node;
 
-	node = malloc(sizeof(t_stack));
+
+	new_node = malloc(sizeof(t_stack));
+	if (!new_node)
+		return (1);
+
+	new_node->content = content;
+		
 	
-	//creer une node
-
-	//la mettre a la fin
-		//la fin de la boucle c'est le moment où (*t_stack** == t_stack*) - 1
-
-
+	if (*first_node == NULL) // ok avec printf
+	{
+		*first_node = new_node;
+		new_node->next = NULL;
+		return (0);
+	}
 	
+	node = *first_node;
 	
-
-
-
+	while (node->next != NULL && node->next != *first_node)
+	 	node = node->next;
+	node->next = new_node;
+	new_node->next = *first_node;
+	return (0);
 }
 
 int	ft_init_stack(char **argv, t_stack **stack_a)
 {
+	int	i;
 	int	tmp;
 
-
-
-	//boucle pour chaque arg
-		//remplir le tmp avoir le nombre char en int (error: !int & int min max)
-			//if nbr > int min int max / 10 return 1
-
+	i = 0;
+	tmp = 0;
+	while (argv[i])
+	{
+		tmp = ft_atoi(argv[i]); ////gestion erreur
+		if (tmp == 123)
+			return (1);
+			
 		//verifier si duplicata
-
-		//create end node
-			//creer une node (int + ->next)
-			//add_back
-
-	//fin boucle
+		
+		if (ft_create_end_node(stack_a, tmp))
+			return (1);
+			
+		i++;
+	}
+	return (0);
 }
 
+t_stack	**ft_create_stack(t_stack **stack)
+{
+	stack = malloc(sizeof(t_stack *));
+	if (!stack)
+	{
+		write(2, "Error\n", 6);
+		return (NULL);
+	}
+	stack = NULL;
+	return (stack);
+}
 
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	
-	printf("Nombres d'arguments: %d\n", argc);
-
-	i = 1;
-	while (i < argc)
-	{
-		printf("Arguments %d: %s\n", argc, argv[i]);
-		i++;
-	}
-
-
 	t_stack	**stack_a;
 	t_stack **stack_b;
 
-	stack_a = malloc(sizeof(t_stack *));
-	if (!stack_a)
-	{
-		write(2, "Error\n", 6);
-		return (1);
-	}
+	if (argc <= 1)
+		return (0);
+
 	
-	stack_b = malloc(sizeof(t_stack *));
-	if (!stack_b)
+	////////////////Creation des deux stacks///////////////////////
+	stack_a = NULL;
+	stack_b = NULL;
+	stack_a = ft_create_stack(stack_a);
+	stack_b = ft_create_stack(stack_b);
+	///////////////////////////////////////////////////////////////
+
+
+
+	
+	//creation liste chainee avec les arguments et gestions d'erreur
+	if (ft_init_stack(&argv[1], stack_a))
 	{
 		write(2, "Error\n", 6);
 		return (1);
 	}
 
-	//creer une liste chainée bouclée / remplir des argv / parsing (error)
-	if (ft_init_stack(argv, stack_a))
-	{
-		write(2, "Error\n", 6);
-		return (1);
-	}
-
-
+	//////////////////////// test stack_a/////////////////////////// 
+	// t_stack *test_first;
+	// test_first = *stack_a;
+	
+	// printf("first node: %p, node: %p, content: %d, next: %p\n", *stack_a, test_first, test_first->content, test_first->next);
+	// while (test_first->next != NULL && test_first->next != *stack_a)
+	// {
+	// 	test_first = test_first->next;
+	// 	printf("first node: %p, node: %p, content: %d, next: %p\n", *stack_a, test_first, test_first->content, test_first->next);
+	// }
+	//////////////////////////////////////////////////////////////////////
 
 
 	//en binaire si 0 stack a / si 1 stack b
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: algasnie <algasnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 15:16:44 by algasnie          #+#    #+#             */
-/*   Updated: 2025/11/17 15:21:41 by algasnie         ###   ########.fr       */
+/*   Updated: 2025/11/18 10:47:06 by algasnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 typedef struct s_stack
 {
 	int content;
-	t_stack next;
+	struct s_stack *next;
 }	t_stack;
 
 
